5826091a1b688494f2fc6c7ea5e0f3f024c9b30d
dbianchii <gdbianchii@gmail.com>
2023-05-02 20:21:26 -0300

    Change cn helper comment

--- src/components/ui/lib/utils.ts
+++ src/components/ui/lib/utils.ts
@@ -1,7 +1,6 @@
-//This file is related to shadcn, used for components. Maybe we can move it to the components folder later.
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
