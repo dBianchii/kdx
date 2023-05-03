0135ac21323d0a404d461f35cda8784bdf42128c
dbianchii <gdbianchii@gmail.com>
2023-05-03 01:15:59 -0300

    Better signin buttons

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import type { InferGetStaticPropsType } from "next";
 import { useState } from "react";
+import { Loader2 } from "lucide-react";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@ui/card";
 import { Label } from "@ui/label";
@@ -17,6 +18,7 @@ function SignIn({ providers }: InferGetStaticPropsType<typeof getStaticProps>) {
   if (session) void router.push("/");
 
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
   return (
     <section>
@@ -49,27 +51,45 @@ function SignIn({ providers }: InferGetStaticPropsType<typeof getStaticProps>) {
                     />
                     <Button
                       variant="default"
-                      onClick={() =>
-                        void signIn("email", { email, callbackUrl: "/" })
-                      }
+                      onClick={() => {
+                        void signIn("email", { email, callbackUrl: "/" });
+                        setLoading(true);
+                      }}
                       className="mt-4"
+                      disabled={loading}
                     >
                       Sign In
                     </Button>
                   </>
                 )}
 
-                <Separator className="my-4" />
+                <div className="relative my-4">
+                  <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t" />
+                  </div>
+                  <div className="relative flex justify-center text-xs uppercase">
+                    <span className="bg-background px-2 text-muted-foreground">
+                      Or continue with
+                    </span>
+                  </div>
+                </div>
 
                 {providers?.google && (
                   <>
                     <Button
-                      variant="ghost"
-                      onClick={() =>
-                        void signIn("google", { callbackUrl: "/" })
-                      }
+                      variant="outline"
+                      onClick={() => {
+                        void signIn("google", { callbackUrl: "/" });
+                        setLoading(true);
+                      }}
+                      disabled={loading}
                     >
-                      <FcGoogle className="mr-2 h-4 w-4" /> Sign in with Google
+                      {loading ? (
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      ) : (
+                        <FcGoogle className="mr-2 h-4 w-4" />
+                      )}
+                      Google
                     </Button>
                   </>
                 )}
