6e1aef1fbbcd1cef65aa4610d80cba363d85381b
gdbianchii <gdbianchii@gmail.com>
2023-05-10 12:11:20 -0300

    Test cron for fun why not

---
+++ src/pages/api/crons/testCron.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  response.status(200).end("Hello Cron!");
+}
