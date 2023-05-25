23bb114d04751bcace1f378efd90f305aadd20d6
gdbianchii <gdbianchii@gmail.com>
2023-05-25 08:59:00 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,15 @@ import SEO from "../components/SEO";
 import { buttonVariants } from "@ui/button";
 import { api } from "@/utils/api";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Home = () => {
-  const { data: session } = useSession();
-  const { data: workspaces } = api.workspace.getAllForLoggedUser.useQuery(
+  const sessionData = useSession();
+  const { data: workspace } = api.workspace.getActiveWorkspace.useQuery(
     undefined,
     {
-      enabled: session?.user !== undefined,
+      enabled: sessionData.data?.user !== undefined,
     }
-  );
-  const workspace = workspaces?.find(
-    (x) => x.id === session?.user?.activeWorkspaceId
   );
 
   return (
@@ -25,13 +23,13 @@ const Home = () => {
         <h1 className="scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-8xl">
           Welcome to Kodix
         </h1>
-        {session && (
+        {sessionData.data && (
           <div className=" text-2xl">
             <p>Your current active workspace is:</p>
             <div className="text-bold inline-flex h-[40px] items-center text-primary">
               <Avatar className="my-auto mr-2 h-5 w-5">
                 <AvatarImage
-                  src={`https://avatar.vercel.sh/${session.user.id}kdx.png`}
+                  src={`https://avatar.vercel.sh/${sessionData.data.user.id}kdx.png`}
                   alt={workspace?.name}
                 />
                 <AvatarFallback>
@@ -48,13 +46,17 @@ const Home = () => {
 
         <div className="flex flex-col items-center">
           <div className="flex flex-col items-center justify-center gap-4">
-            <Link
-              href={session ? "" : "/signIn"}
-              onClick={session ? () => void signOut() : () => null}
-              className={buttonVariants({ variant: "default", size: "lg" })}
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
+            {sessionData.status === "loading" ? (
+              <Skeleton className="h-12 w-32" />
+            ) : (
+              <Link
+                href={sessionData.data ? "" : "/signIn"}
+                onClick={sessionData.data ? () => void signOut() : () => null}
+                className={buttonVariants({ variant: "default", size: "lg" })}
+              >
+                {sessionData.data ? "Sign out" : "Sign in"}
+              </Link>
+            )}
           </div>
         </div>
       </div>
