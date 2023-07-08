68d364f07030c47394f174df6a37cf11154a4e72
gdbianchii <gdbianchii@gmail.com>
2023-07-08 18:00:19 -0300

    test edge middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from "next/server";
 // Trigger this middleware to run on the `/secret-page` route
 export const config = {
   matcher: "/testMiddleware",
+  runtime: "edge",
 };
 
 export function middleware(request: NextRequest) {
